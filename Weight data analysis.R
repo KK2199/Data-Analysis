d7087a906fd010979478d1bf7851cd2781e1381b
KK2199 <kk2198@bath.ac.uk>
2025-04-30 08:59:35 +0100

    Add files via upload

--- Weight data analysis.R
+++ Weight data analysis.R
@@ -1,5 +1,7 @@
 #To address the research question: does weight differ between genotypes (at x weeks/months)?
 #One way ANCOVA
+#Problem I am having with this is I'd like to include all data and just get rid of NAs, 
+#but I can't get rid of NAs without getting rid of the whole row for a mouse.
 
 library(readxl)
 library(ggplot2)
@@ -14,23 +16,33 @@ library(emmeans)
 #Converting the variables into the right format
 df <- read_xlsx("All weight data.xlsx")
 df$genotype <- as.factor(df$genotype)
-df$mass_numeric <- as.numeric(df$mass_15w)
-df$sex_numeric <- ifelse(df$sex == "male", 1, 0)
-
-  
-str(df$sex_numeric)
-
+df$sex <- as.factor(df$sex)
+df$mass_15w <- as.numeric(df$mass_15w)
+df$mass_7m <- as.numeric(df$mass_7m)
+df$mass_10m <- as.numeric(df$mass_10m)
 
 head(df)
 summary(df)
 
-df_clean <- df %>%
-  dplyr::filter(is.finite(sex_numeric) & is.finite(mass_numeric))
 
+#Converting data to long format
+long_data <- pivot_longer(clean_data, 
+                          cols = starts_with("mass"), 
+                          names_to = "age", 
+                          names_prefix = "mass_",
+                          values_to = "mass")
+print(long_data)
+
+summary(long_data)
+
+long_data_clean <- long_data %>%
+  filter(!is.na(mass), !is.na(genotype), !is.na(sex))
+
+print(long_data_clean)
 
 #Linearity assumptions for sex covariate
 ggscatter(
-  df_clean, x = "sex_numeric", y = "mass_numeric",
+  long_data, x = "sex", y = "mass",
   colour = "genotype", add = "reg.line"
 ) +
   stat_regline_equation(
@@ -39,12 +51,12 @@ ggscatter(
   )
 
 #Homogenity of regression slopes 
-df %>%
-  anova_test(mass_numeric ~ genotype*sex_numeric)
+long_data %>%
+  anova_test(mass ~ genotype*sex, na.action = na.omit)
 
 
 #Normality of the residuals
-model <- lm(mass_numeric ~ sex_numeric + genotype, data = df)
+model <- lm( ~ sex + genotype, data =long_data)
 model.metrics <- augment(model) %>%
   select(-.hat, -.sigma, -.fitted)
 head(model.metrics, 3)
@@ -64,22 +76,27 @@ model.metrics %>%
 
 #There are outliers
 
+
 res.aov <- df %>%
-  anova_test(mass_numeric ~ sex_numeric + genotype)
+  anova_test( ~ sex + genotype)
 get_anova_table(res.aov)
 
 
-# Post hoc test - I can't get this bit of code to work!
+# Post hoc test 
 
-pwc <- df %>%
-  emmeans_test(mass_numeric ~ genotype, covariate = sex_numeric,
-               p.adjust.method = "bonferroni")
-pwc
+emm <- emmeans(model, ~ genotype)
 
-get_emmeans(pwc)
+pwc <- contrast(emm, method = "pairwise", adjust = "bonferroni")
+summary(pwc)
 
-#To address the research question: does weight differ between the genotypes and does this change with age?
-#Linear Mixed Effects Model
+emm_df <- as.data.frame(emm)
+
+
+
+
+
+
+
 
 
 
